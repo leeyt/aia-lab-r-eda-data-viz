5c0e4e5c719a91854a68ab33a8ca6cf6d52c7611
Neil Lee <neil.lee@udngroup.com.tw>
2018-03-11 15:44:00 +0800

    新增中文註解，移除重複的英文註解

--- R_basis_DataTypes_and_VectorOperation.R
+++ R_basis_DataTypes_and_VectorOperation.R
@@ -4,38 +4,38 @@ R 語言探索性資料分析與資料視覺化
 '
 
 # 將此行轉向你的程式碼放置路徑
-# Target to the folder where you put the codes
-setwd("D:/Dropbox/Sinica_Project/Theta/AIA/lectures")
+setwd("~/ai-academy/aia-lab-r-eda-data-viz")
 
-### Number and String ====
+### Numberic (數值型態) and Character (字元型態)
 
 this.is.a.numeric_item <- 101
-this.is.a.numeric_item2 <- c(101,201,301,101)
-this.is.a.character_item <- "101" # to define a character variable, use bracket or double bracket
+this.is.a.numeric_item2 <- c(101, 201, 301, 101)
+
+# 要定義文字變數，可使用單引號或雙引號
+this.is.a.character_item <- '101'
 this.is.a.character_item2 <- "abcbabd"
 
-# function: typeof
-# 列出一個變數 (物件) 的屬性
-# Show the type of a variable (object)
+'
+function: typeof
+列出一個變數 (物件) 的資料型態
+'
 typeof(this.is.a.numeric_item)
 typeof(this.is.a.numeric_item2)
 typeof(this.is.a.character_item)
 typeof(this.is.a.character_item2)
 
-
-# 數值變數才可以進行數學運算
-# Only numeric variable can do mathematical operation
+# 只有數值變數才可以進行數學運算
 this.is.a.numeric_item + 101
-this.is.a.numeric_item2[2] + 101
+this.is.a.numeric_item2[2] + 101   # 索引值從1開始
 print(this.is.a.character_item)
-this.is.a.character_item + 101 # this line will make a error
+this.is.a.character_item + 101     # 這行會報錯
 
 print(this.is.a.numeric_item)
 print(this.is.a.character_item)
-this.is.a.numeric_item + this.is.a.character_item # this line will make a error
+this.is.a.numeric_item + 
+  this.is.a.character_item         # 這行會報錯
 
 # 數字型的字串變數可以被轉為數值變數, 非數字型文字會變成 NA
-# A numeric-like character variables can be converted to a numeric variable. Non-numeric-like character will become NA.
 print(this.is.a.character_item)
 this.is.a.numeric_item.from.character <- as.numeric(this.is.a.character_item)
 print(this.is.a.numeric_item.from.character)
@@ -44,12 +44,11 @@ this.is.a.numeric_item2.from.character2 <- as.numeric(this.is.a.character_item2)
 print(this.is.a.numeric_item2.from.character2)
 
 typeof(this.is.a.numeric_item.from.character)
-typeof(this.is.a.numeric_item2.from.character2) # however, NA still be seen as double
+typeof(this.is.a.numeric_item2.from.character2) # 不過，NA 仍會被視為 double
 
 this.is.a.numeric_item + this.is.a.numeric_item.from.character
 
-### 警告: 任何數字對 NA 進行數學操作，都會返回 NA
-### Warning: any math operation with NA will return NA
+### 警告: 任何數字跟 NA 進行數學運算，都將回傳 NA
 1 + NA
 print(this.is.a.numeric_item)
 print(this.is.a.numeric_item2.from.character2)
@@ -60,110 +59,96 @@ this.is.a.numeric_list2 <- c(1,2,3,4, NA)
 
 sum(this.is.a.numeric_list)
 sum(this.is.a.numeric_list2)
-sum(this.is.a.numeric_list2, na.rm = TRUE)
+sum(this.is.a.numeric_list2, na.rm=TRUE)
 
-# 檢視變數列中，是否有 NA (以及NA 在哪)
-# Check NA in the variable list
+# 檢視變數列表中，是否有 NA (以及 NA 在哪)
 print(this.is.a.numeric_list2)
 is.na(this.is.a.numeric_list2)
 which(is.na(this.is.a.numeric_list2))
 
-# 字串形式可以被用任意字符切開
-# Character can be splited by a character
+# 字串可以用任意字符切開
 print(this.is.a.character_item2)
-strsplit(x = this.is.a.character_item2, split = "b")
+strsplit(x=this.is.a.character_item2, split="b")
 
-# 尋找特定字眼是否出現在變數列中
-# Find specific term in the variable list
+# 尋找特定字眼是否出現在變數列表中
 temp <- strsplit(x = this.is.a.character_item2, split = "b")[[1]]
 print(temp)
 "c" %in% temp
-"C" %in% temp
+"C" %in% temp               # 字串比對會看大小寫
 tolower("C") %in% temp
 
-
-### Factor ====
+### Factor (因子)
 '
 Factor 的主要目的: 省空間 -- numeric vector + levels
-The main idea of factor is to use memory efficiently -- numeric vector + levels
 '
 this.is.a.character_list <- c("A","B","C","B","A","A")
-
 this.is.a.character_to_factor <- factor(this.is.a.character_list)
+
 str(this.is.a.character_to_factor)
 levels(this.is.a.character_to_factor)
 
-# 因子可以是有順序性的
-# Factor can be ordered by add "ordered = TRUE, levels = ..."
-this.is.a.character_to_factor2 <- factor(this.is.a.character_list, 
-                                         ordered = TRUE, 
-                                         levels = c("C","B","A"))
+# 因子可以是有順序性的，只要加上 ordered = TRUE
+this.is.a.character_to_factor2 <- 
+  factor(x=this.is.a.character_list, 
+         ordered = TRUE,
+         levels = c("C","B","A"))
+
 str(this.is.a.character_to_factor2)
 levels(this.is.a.character_to_factor2)
 
-### list ====
+### List (列表)
 # 建立一個含有多種資料型態在內的 list 
-# Create a list with several data types inside
 
 this.is.a.list <- list(100,
                        c(200,300,400),
                        'a',
-                       c('cat','dog')
-                       )
+                       c('cat','dog'))
 print(this.is.a.list)
 
 # 用 [[ ]] 取得 list 中的 element
-# To access the element in the list, use [[ ]]
 for (i in seq(length(this.is.a.list))) {
-  # print the ith element in the list
+  # 印出 list 的第 i 個元素
   print(this.is.a.list[[i]]) 
 }
 
-
-### data.frame ====
+### data.frame (資料框)
 mtcars
 
-# 檢視資料集的前 n 行與後 n 行
-# Examine the first/last few rows of the data frame
-head(mtcars, 5)
-tail(mtcars) # n defaulf is 6
+# 檢視資料集的前 n 列與後 n 列
+head(mtcars, n=5)
+tail(mtcars)       # n 預設值為 6
 
 # 檢視資料的列名稱與欄名稱
-# Examine the row names and column names of the data frame
 rownames(mtcars)
 colnames(mtcars)
 
-# 檢視資料集中，每個欄位的資料型態
-# Examine the type of the data frame of each column
+# 檢視資料框中，每個欄位的資料型態
 typeof(mtcars$mpg)
+typeof(mtcars['mpg'])
 
 ?sapply
 sapply(mtcars, typeof)
 
-# 檢視資料集的摘要
-# Examine the basic infomation and summary of the data frame
+# 檢視資料框的基本資訊及摘要
 str(mtcars)
 summary(mtcars)
 
-# 資料集中的某欄數值可以透過 $ 記號取得，或是用 "var"
-# access a variable of data frame through $ or "var"
-mtcars$mpg # will return vector
-mtcars[, "mpg"] # will return vector
-mtcars["mpg"] # will return a data frame
+# 資料框中的某欄數值可以透過 $ 記號取得，或是用 ["var"]
+mtcars$mpg       # 以向量形式回傳
+mtcars[, "mpg"]  # 以向量形式回傳
+mtcars["mpg"]    # 以 data.frame 形式回傳
 
 # 也可以透過 $ 創造新的欄位
-# create new column through $
 mtcars$hp.mpg <- mtcars$hp / mtcars$mpg
 print(head(mtcars[c("hp", "mpg", "hp.mpg")]))
 
 # 選取特定列與欄資料
-# access certain row and columns
-mtcars[5, ] # display the 5th row of the data frame
-mtcars[c(2,3,4), ] # display the 2 to 4 rows of the data frame
+mtcars[5, ]        # 顯示 data.frame 的第 5 列
+mtcars[c(2,3,4), ] # 顯示 data.frame 的第 2,3,4 列
 
-mtcars[c(2,3,4), c("mpg","cyl")]
+mtcars[c(2,3,4), c("mpg","cyl")] # 顯示指定列欄的交集
 
 ### END  ==== 
-# This is the end of this section
+
 rm(list = ls()) # clear all variables
 
